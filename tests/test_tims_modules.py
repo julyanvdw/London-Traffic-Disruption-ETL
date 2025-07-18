db6051c2141b1b4ba2f871d48ab60a32c129ff47
Julyan <vwsjul003@myuct.ac.za>
2025-07-18 11:36:50 +0200

    added tests for pydantic objects

--- tests/test_tims_modules.py
+++ tests/test_tims_modules.py
@@ -2,7 +2,7 @@
 Julyan van der Westhuizen
 17/07/25
 
-This scrip runs tests using PyTest to test all TIMS related modules
+This scrip runs tests using PyTest to test all TIMS-related modules
 """
 
 import sys
@@ -134,3 +134,83 @@ def test_LakeManager_read_TIMS_transformed_snapshot():
         data = manager.read_TIMS_transformed_snapshot()
         assert len(data) == 1
         assert data[0] == [disruption.model_dump()]
+
+# Pydantic model tests
+def test_Disruption_parsing():
+    # Checks if the pydantic model parses JSON as expected (strips what's not defined and keeps what is)
+
+    fake_data = [
+        {
+            "$type": "Tfl.Api.Presentation.Entities.RoadDisruption, Tfl.Api.Presentation.Entities", 
+            "id": "TIMS-214298", 
+            "url": "/Road/All/Disruption/TIMS-214298", 
+            "point": "[0.119257,51.568495]", 
+            "severity": "Serious", 
+            "ordinal": 1, 
+            "category": "Collisions"
+        }
+    ]
+
+    disruption = tims_models.Disruption(**fake_data[0])
+
+    assert disruption.tims_id == "TIMS-214298"
+    assert disruption.url == "/Road/All/Disruption/TIMS-214298"
+    assert disruption.severity == "Serious"
+    assert disruption.ordinal == 1
+    assert disruption.category == "Collisions"
+
+def test_Disruption_optionality():
+    # Checks if the pydantic object still parses even though it doesn't have some of its optional fields
+
+    fake_data = [
+        {
+            "id": "TIMS-214298", #needs to include
+            # lack of other, optional, fields
+        }
+    ]
+
+    disruption = tims_models.Disruption(**fake_data[0])
+    assert disruption.tims_id == "TIMS-214298"
+    assert disruption.url == None #properly assigned None to a field not given
+    
+def test_Disruption_valid_id(): 
+    # Checks if the disruption object properly checks valid IDs
+    disruption = tims_models.Disruption(id="TIMS-214298")
+    assert disruption.tims_id == "TIMS-214298"
+
+def test_Disruption_invalid_id():
+    # Checks if the disryption object properly checks for INVALID IDs
+    with pytest.raises(Exception):
+        disruption = tims_models.Disruption(id="SOME INVALID ID")
+
+def test_Disruption_missing_id():
+    # Checks if the parsing fails when the REQUIRED ID is not there
+    with pytest.raises(Exception):
+        tims_models.Disruption()
+
+def test_invalid_ordinal_type():
+    # Checks proper type checking with the example field of ordinal
+    with pytest.raises(Exception):
+        tims_models.Disruption(id="TIMS-214298", ordinal="stringwhenitshouldbeint")
+
+def test_nested_structure():
+    # Checks if the pydantic model correctly parses nested JSON structures as per model def
+    segment_data = {"toid": "0"}
+    street_data = [{"name": "Buitengracht Street", "segments": [segment_data]}]
+    disruption = tims_models.Disruption(id="TIMS-214298", streets=street_data)
+
+    # Check that it worked
+    assert disruption.streets[0].name == "Buitengracht Street"
+    assert disruption.streets[0].segments[0].toid == "0"
+
+def test_StreetSegment_convert_to_list_validator():
+    # Checks that a lineString can be converted into a list of lists of floats
+    fake_lineString = "[[0.1, 51.5], [0.2, 51.6]]"
+    segment = tims_models.StreetSegment(lineString=fake_lineString)
+    assert segment.coords == [[0.1, 51.5], [0.2, 51.6]]
+
+def test_StreetSegment_coords_invalid_string():
+    # Checks that non-valid lineStrings just get passed as None
+    fake_lineString = 'some invalid lineString'
+    segment = tims_models.StreetSegment(lineString=fake_lineString)
+    assert segment.coords is None

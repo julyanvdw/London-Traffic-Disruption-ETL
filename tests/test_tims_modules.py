5e7a07e931536a081c452f84ec27faed854c7457
Julyan <vwsjul003@myuct.ac.za>
2025-07-18 10:51:06 +0200

    Added pytests for datalake_manager

--- tests/test_tims_modules.py
+++ tests/test_tims_modules.py
@@ -5,13 +5,17 @@ Julyan van der Westhuizen
 This scrip runs tests using PyTest to test all TIMS related modules
 """
 
-import pytest
 import sys
 sys.path.append("../")
 from unittest.mock import patch, Mock
 import requests
-
+import tempfile
+import os
+import json
+import pytest
 from extract import fetch_TIMS
+from datalake_manager import LakeManager
+from tranform import tims_models
 
 # EXTRACTING TESTS
 
@@ -53,4 +57,80 @@ def test_fetch_TIMS_snapshot_writing():
 
         mock_manager_instance.write_TIMS_raw_snapshot.assert_called_once_with([{"id": "TIMS-206772"}])
 
-    
+# data_manager TESTS
+
+def test_LakeManager_write_TIMS_raw_snapshot():
+    # Checks if LakeManager properly writes raw snapshots to a real file (in a temp dir)
+    with tempfile.TemporaryDirectory() as tmpdir:
+        manager = LakeManager()
+        manager.tims_raw_dir_location = tmpdir  #setting it up so that it uses a fake temporary dir
+
+        test_data = [{"id": "TIMS-206772"}]
+        manager.write_TIMS_raw_snapshot(test_data)
+
+        # check that the file was created and that the contents are the same as that of the test data
+        files = os.listdir(tmpdir)
+        assert len(files) == 1
+        filepath = tmpdir + "/" + files[0]
+        with open(filepath, "r") as f:
+            data = json.load(f)
+        assert data == test_data
+
+def test_LakeManager_read_TIMS_raw_snapshot():
+    # Checks if LakeManager can properly read the contents of a specified directory and return the appropriate data
+    with tempfile.TemporaryDirectory() as tmpdir: 
+        # create fake file to read
+
+        test_data = [{"id": "TIMS-206772"}]
+
+        with open(f"{tmpdir}/fake.json", "w") as f:
+            json.dump(test_data, f)
+        
+        # Setup manager
+        manager = LakeManager()
+        manager.tims_raw_dir_location = tmpdir
+        data = manager.read_TIMS_raw_snapshot()
+
+        assert len(data) == 1
+        assert data[0] == test_data
+
+def test_LakeManager_write_TIMS_transformed_snapshot():
+    # Checks if the LakeManager properly writes transformed snapshots to a real file (in a temp dir)
+    with tempfile.TemporaryDirectory() as tmpdir: 
+        manager = LakeManager()
+        manager.tims_transformed_dir_location = tmpdir
+
+
+        # create some test data
+        test_item = {"id": "TIMS-206772"}
+        disruption = tims_models.Disruption(**test_item)
+        test_data = [disruption]
+        manager.write_TIMS_transformed_snapshot(test_data)
+
+        # check if the writing was successful
+        files = os.listdir(tmpdir)
+        assert len(files) == 1 #only one file written
+        filepath = tmpdir + "/" + files[0]
+        with open(filepath, "r") as f:
+            data = json.load(f)
+            assert len(data) == 1
+        assert data == [test_data[0].model_dump()]
+
+def test_LakeManager_read_TIMS_transformed_snapshot():
+    # Checks if the LakeManager properly reads the transformed snapshots
+    with tempfile.TemporaryDirectory() as tmpdir:
+        # setup manager
+        manager = LakeManager()
+        manager.tims_transformed_dir_location = tmpdir
+
+        # Create a fake pydantic object
+        disruption = tims_models.Disruption(id="TIMS-206772")
+
+        # write some fake data for it to read
+        with open(f"{tmpdir}/fake.json", "w") as f:
+            json.dump([disruption.model_dump()], f, indent=2, default=str)
+
+        # test the function
+        data = manager.read_TIMS_transformed_snapshot()
+        assert len(data) == 1
+        assert data[0] == [disruption.model_dump()]

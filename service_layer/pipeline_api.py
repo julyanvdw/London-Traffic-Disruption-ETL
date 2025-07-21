1f58bf1af353c1df000cd2c99da592a10eaf4c69
Julyan <vwsjul003@myuct.ac.za>
2025-07-21 18:49:42 +0200

    Setup service layer structure

---
+++ service_layer/pipeline_api.py
@@ -0,0 +1,8 @@
+"""
+Julyan van der Westhuizen
+21/07/25
+
+This script outlines a FastAPI python API created for safe access to the Postgres database.
+The aim is to provide a 'Sevice Layer' to the data pipeline - completing the full lifecycle of data.
+"""
+

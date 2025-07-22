884f8df0347d0ed92617dd720f891e414bc5e51a
Julyan <vwsjul003@myuct.ac.za>
2025-07-22 12:22:57 +0200

    basic plotting with plotly

--- service_layer/example_app.py
+++ service_layer/example_app.py
@@ -5,10 +5,13 @@ Julyan van der Westhuizen
 
 This script details an example application which uses API endpoints from the service layer.
 As a simulation, this application represents the last phase of the data-pipline process: usage
+
+For this example application, we'll be plotting disruptions in London over a period of time. We'll also do some stats at each point.
 """
 
 import pandas as pd
 import requests
+import plotly.express as px
 
 # CONNECTING TO MY LOCAL API
 
@@ -31,5 +34,18 @@ for index in df.index:
     df.at[index, "longitude"] = coords[0]
     df.at[index, "latitude"] = coords[1]
 
-print(df[["longitude", "latitude"]])
+# PLOT USING PLOTLY
+
+fig = px.scatter_mapbox(
+    df,
+    lat="latitude",
+    lon="longitude",
+    hover_name="description" if "description" in df.columns else None,
+    color="status" if "status" in df.columns else None,
+    zoom=10,
+    height=600
+)
+fig.update_layout(mapbox_style="open-street-map")
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig.show()
 

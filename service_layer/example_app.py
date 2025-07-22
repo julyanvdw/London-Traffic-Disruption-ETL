e40939312a3c82ede6aa9098548d40880182606a
Julyan <vwsjul003@myuct.ac.za>
2025-07-22 14:03:22 +0200

    Example app done for now

--- service_layer/example_app.py
+++ service_layer/example_app.py
@@ -37,8 +37,7 @@ for index in df.index:
 df["snapshot_time"] = pd.to_datetime(df["snapshot_time"])
 df["snapshot_minute"] = df["snapshot_time"].dt.strftime('%Y-%m-%d %H:%M')
 
-# PLOT USING PLOTLY WITH TIMELINE ANIMATION
-
+# PLOT ANIMATION USING PLOTLY WITH TIMELINE ANIMATION
 
 fig = px.scatter_mapbox(
     df,
@@ -48,11 +47,54 @@ fig = px.scatter_mapbox(
     hover_data=["snapshot_time", "severity", "category", "subcategory", "comments", "currentupdate", "levelofinterest", "location", "status"],
     animation_frame="snapshot_minute",
     zoom=10,
-    height=600
+    height=800
 )
 
 fig.update_traces(marker=dict(size=15, opacity=0.8))
-fig.update_layout(mapbox_style="open-street-map")
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig.update_layout(
+    mapbox_style="open-street-map", 
+    title={
+        'text': "London Traffic Disruptions Over Time",
+        'y':0.95,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'
+    })
+
+# CALCULATE STATISTICS FOR THIS API PULL - add as annotation above map
+
+stats = []
+total_unique = df['tims_id'].nunique()
+stats.append("Total unique disruptions: " + str(total_unique))
+
+stats.append("Disruptions' status:")
+status_counts = df['status'].value_counts()
+for i in status_counts.index:
+    stats.append(str(i) + ": " + str(status_counts[i]))
+
+if 'category' in df.columns:
+    stats.append("Disruptions per category:")
+    category_counts = df['category'].value_counts()
+    for k in category_counts.index:
+        stats.append(str(k) + ": " + str(category_counts[k]))
+
+first_snapshot = df['snapshot_time'].min()
+last_snapshot = df['snapshot_time'].max()
+stats.append("First snapshot time: " + str(first_snapshot))
+stats.append("Last snapshot time: " + str(last_snapshot))
+stats_text = '<br>'.join(stats) #for the plotly displaying
+
+fig.add_annotation(
+    text=stats_text,
+    xref="paper", yref="paper",
+    x=0, y=0.9, showarrow=False,  # move annotation further down
+    align="left",
+    font=dict(size=14),
+    bordercolor="black",
+    borderwidth=1,
+    bgcolor="white",
+    opacity=0.8
+)
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":180})  # increase bottom margin
 fig.show()
 
